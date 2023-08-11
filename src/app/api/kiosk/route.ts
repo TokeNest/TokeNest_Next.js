84efa76c7954906507f6d02d67da37966076278e
kecan0406 <kecan0406@gmail.com>
2023-08-12 00:43:51 +0900

    *feat : swagger setting

--- src/app/api/kiosk/route.ts
+++ src/app/api/kiosk/route.ts
@@ -2,6 +2,15 @@ import { mockProductData } from '@/variables/kiosk-api-mock'
 import { Request } from 'next/dist/compiled/@edge-runtime/primitives'
 import { NextResponse } from 'next/server'
 
+/**
+ * @swagger
+ * /api/kiosk:
+ *   get:
+ *     description: nice
+ *     responses:
+ *       200:
+ *         description: nice 2 meet you
+ */
 export async function GET(request: Request) {
   return NextResponse.json({ data: mockProductData })
 }
