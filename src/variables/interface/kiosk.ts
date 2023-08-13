2976d2a98e9775f74cd7228b6d695ed12b2a5418
kecan0406 <kecan0406@gmail.com>
2023-08-13 16:10:15 +0900

    *docs : interface, enum add comment

--- src/variables/interface/kiosk.ts
+++ src/variables/interface/kiosk.ts
@@ -1,5 +1,8 @@
 import { TOKEN } from '@/web3/contractList'
 
+/**
+ * Product에 관한 정보
+ * */
 export interface Product {
   tokenAddress: TOKEN
   tokenRatio: number
@@ -10,18 +13,28 @@ export interface Product {
   imageUrl: string
   options: ProductOption[]
 }
+
+/**
+ * Product에서 카테고리 분류
+ * */
 export interface Category {
   name: string
   index: number
   data: Product[]
 }
 
+/**
+ * Product에서 사용하는 옵션 정보
+ * */
 export interface ProductOption {
   optionName: string
   defaultValue: number
   optionInfo: OptionInfo[]
 }
 
+/**
+ * 옵션 구성
+ * */
 export interface OptionInfo {
   label: string
   value: number
--- src/variables/interface/web3.ts
+++ src/variables/interface/web3.ts
@@ -1,10 +1,17 @@
 import { MARKET, TOKEN } from '@/web3/contractList'
 
+/**
+ * 토큰에 관한 정보.
+ * */
 export interface TokenInfo {
   address: TOKEN
   decimal: number
   price: string
 }
+
+/**
+ * 마켓에 관한 정보.
+ * */
 export interface MarketInfo {
   market: MARKET
   tokenA: TokenInfo
