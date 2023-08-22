0600ee2068882615bc374d218743d5cfe71cd1f6
kecan0406 <kecan0406@gmail.com>
2023-08-22 23:17:38 +0900

    fix : cart drawer layout modify

--- src/components/kiosk/KioskDrawer/cart/KioskCartBody.tsx
+++ src/components/kiosk/KioskDrawer/cart/KioskCartBody.tsx
@@ -19,79 +19,88 @@ import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
 
 export default function KioskCartBody() {
+  return (
+    <CardContent sx={{ height: 8 / 10 }}>
+      <List>
+        <KioskListItem />
+        <KioskListItem />
+      </List>
+    </CardContent>
+  )
+}
+
+function KioskListItem() {
   const [open, setOpen] = useState(false)
   const handleOpen = () => setOpen(!open)
   return (
-    <CardContent>
-      <List>
-        <ListItem
-          secondaryAction={
-            <IconButton edge="end" aria-label="open" onClick={handleOpen}>
-              <KeyboardArrowDown
+    <>
+      <ListItem
+        secondaryAction={
+          <IconButton edge="end" aria-label="open" onClick={handleOpen}>
+            <KeyboardArrowDown
+              sx={{
+                transform: `rotate(${open ? 180 : 0}deg)`,
+                transition: '0.2s',
+              }}
+            />
+          </IconButton>
+        }
+      >
+        <ListItemAvatar>
+          <Avatar>
+            <ImageIcon />
+          </Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary={
+            <>
+              <Typography variant="h4" fontWeight="bold">
+                아메리카노
+              </Typography>
+              <Box
                 sx={{
-                  transform: `rotate(${open ? 180 : 0}deg)`,
-                  transition: '0.2s',
+                  display: 'flex',
+                  alignItems: 'center',
+                  flexGrow: 1,
                 }}
-              />
-            </IconButton>
+              >
+                <CloseIcon fontSize="large" color="action" sx={{ mx: 1 }} />
+                <Typography variant="h4">3</Typography>
+              </Box>
+              <Typography variant="h4" align="right" fontWeight="bold">
+                7,880원
+              </Typography>
+            </>
           }
-        >
-          <ListItemAvatar>
-            <Avatar>
-              <ImageIcon />
-            </Avatar>
-          </ListItemAvatar>
-          <ListItemText
-            primary={
-              <React.Fragment>
-                <Typography variant="h4" fontWeight="bold">
-                  아메리카노
-                </Typography>
-                <Box
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    flexGrow: 1,
-                  }}
-                >
-                  <CloseIcon fontSize="large" color="action" sx={{ mx: 1 }} />
-                  <Typography variant="h4">3</Typography>
-                </Box>
-                <Typography variant="h4" align="right" fontWeight="bold">
-                  7,880원
-                </Typography>
-              </React.Fragment>
-            }
-            sx={{ '& .MuiTypography-body1': { display: 'flex' } }}
-          />
-        </ListItem>
-        <Collapse in={open} timeout="auto" unmountOnExit>
-          <List sx={{ pl: 4, '& .MuiTypography-body1': { display: 'flex' } }}>
-            <ListItem>
-              <ListItemText
-                primary={
-                  <React.Fragment>
-                    <Typography variant="h6">사이즈</Typography>
-                    <Box
-                      sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <HorizontalRuleIcon fontSize="small" color="action" sx={{ mx: 1 }} />
-                      <Typography>VENTI</Typography>
-                    </Box>
-                  </React.Fragment>
-                }
-              />
-            </ListItem>
-            <Divider />
-            <ListItem>
-              <ListItemText primary="사이즈" />
-            </ListItem>
-          </List>
-        </Collapse>
-      </List>
-    </CardContent>
+          sx={{ '& .MuiTypography-body1': { display: 'flex' } }}
+        />
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List sx={{ pl: 4, '& .MuiTypography-body1': { display: 'flex' } }}>
+          <ListItem>
+            <ListItemText
+              primary={
+                <>
+                  <Typography variant="h6">사이즈</Typography>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <HorizontalRuleIcon fontSize="small" color="action" sx={{ mx: 1 }} />
+                    <Typography>VENTI</Typography>
+                  </Box>
+                </>
+              }
+            />
+          </ListItem>
+          <Divider />
+          <ListItem>
+            <ListItemText primary="사이즈" />
+          </ListItem>
+        </List>
+      </Collapse>
+    </>
   )
 }
