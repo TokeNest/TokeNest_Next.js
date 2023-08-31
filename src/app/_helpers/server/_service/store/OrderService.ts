9aafbf143889e7141e96d07e9d2d20254232dc11
choiht0904 <choiht0904@naver.com>
2023-09-01 00:23:56 +0900

    Fix : add validate order api

--- src/app/_helpers/server/_service/store/OrderService.ts
+++ src/app/_helpers/server/_service/store/OrderService.ts
@@ -1,12 +1,26 @@
 import { OrderInfoCreate } from '@/variables/interface/api/order-interface'
 import { orderRepository } from '@/app/_helpers/server/_repository/store/OrderRepository'
 import { orderOptionRepository } from '@/app/_helpers/server/_repository/store/OrderOptionRepository'
+import { storeRepository } from '@/app/_helpers/server/_repository/store/StoreRepository'
+import { productRepository } from '@/app/_helpers/server/_repository/store/ProductRepository'
+import { productOptionRepository } from '@/app/_helpers/server/_repository/store/ProductOptionRepository'
 
-const createOrder = async (params: OrderInfoCreate) => {
+const createOrder = async (params: OrderInfoCreate): Promise<string> => {
+  if (!(await storeRepository.getById(params.store))) {
+    return Promise.reject('Store Not Found')
+  }
   const id = await orderRepository.create(params)
   const optionIds: string[] = []
   await Promise.all(
     params.orderOptions.map(async (orderOption) => {
+      if (!(await productRepository.getById(orderOption.product))) {
+        return Promise.reject('Product Not Found')
+      }
+      orderOption.productOptions.map(async (productOption) => {
+        if (!(await productOptionRepository.getById(productOption))) {
+          throw 'ProductOption Not Found'
+        }
+      })
       optionIds.push(await orderOptionRepository.create(orderOption))
     })
   )
