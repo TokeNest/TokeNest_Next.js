44b6cf7f1571bba4d6fe92e410ebaa840d5aa78d
kecan0406 <kecan0406@gmail.com>
2023-09-02 19:25:58 +0900

    refactor : fix product card componet structor

--- src/app/kiosk/_components/productCard/ProductCard.tsx
+++ src/app/kiosk/_components/productCard/ProductCard.tsx
@@ -2,42 +2,77 @@ import * as React from 'react'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
-import { CardHeader, CardMedia } from '@mui/material'
+import { Box, CardHeader, CardMedia } from '@mui/material'
 import Image from 'next/image'
 import {
   ProductCardActionArea,
   ProductCardPrice,
 } from '@/app/kiosk/_components/productCard/interaction'
 import { ProductInfoClient } from '@/variables/interface/api/product-interface'
+import cuteMockCat from '/public/images/cuteMockCat.png'
+import { FileInfo } from '@/variables/interface/api/file-interface'
+import { ProductOptionGroupInfoClient } from '@/variables/interface/api/product-option-group-interface'
 
 export default function ProductCard({ product }: { product: ProductInfoClient }) {
-  const fileUrl = product.file === null ? '' : product.file?.filePath
   return (
     <Card sx={{ borderRadius: 3, p: 0, width: 200, height: 350 }}>
       <ProductCardActionArea product={product}>
-        <CardMedia sx={{ p: 1, height: 200 }}>
-          <div style={{ position: 'relative', width: '100%', height: '100%', overflow: 'hidden' }}>
-            <Image
-              fill
-              src={fileUrl}
-              alt={product.file?.fileName ?? ''}
-              sizes="(max-width: 200px) 100vw"
-              style={{ objectFit: 'cover' }}
-            />
-          </div>
-        </CardMedia>
-        <CardHeader
-          sx={{ px: 1, py: 0 }}
-          titleTypographyProps={{ width: 184, noWrap: true }}
-          title={product.productName}
-          subheader={<ProductCardPrice product={product} />}
+        <ProductImage file={product.file} />
+        <ProductInformation
+          productName={product.productName}
+          productPrice={product.productPrice}
+          productOptionGroups={product.productOptionGroups}
         />
-        <CardContent>
-          <Typography variant="body1" color="text.primary" sx={{ height: 48, overflow: 'hidden' }}>
-            {product.productIntro}
-          </Typography>
-        </CardContent>
+        <ProductIntro intro={product.productIntro} />
       </ProductCardActionArea>
     </Card>
+  )
+}
+
+function ProductImage({ file }: { file: FileInfo }) {
+  const [src, alt] = file ? [file.filePath, file.fileName] : [cuteMockCat.src, 'not found image']
+  return (
+    <CardMedia sx={{ p: 1, height: 200 }}>
+      <Box sx={{ borderRadius: 3, position: 'relative', width: 1, height: 1 }}>
+        <Image
+          fill
+          src={src}
+          alt={alt}
+          sizes="(max-width: 200px) 100vw"
+          style={{ objectFit: 'cover' }}
+        />
+      </Box>
+    </CardMedia>
+  )
+}
+
+function ProductInformation({
+  productName,
+  productPrice,
+  productOptionGroups,
+}: {
+  productName: string
+  productPrice: number
+  productOptionGroups: ProductOptionGroupInfoClient[]
+}) {
+  return (
+    <CardHeader
+      sx={{ px: 1, py: 0 }}
+      titleTypographyProps={{ width: 184, noWrap: true }}
+      title={productName}
+      subheader={
+        <ProductCardPrice productPrice={productPrice} productOptionGroups={productOptionGroups} />
+      }
+    />
+  )
+}
+
+function ProductIntro({ intro }: { intro: string }) {
+  return (
+    <CardContent>
+      <Typography variant="body1" color="text.primary" sx={{ height: 48, overflow: 'hidden' }}>
+        {intro}
+      </Typography>
+    </CardContent>
   )
 }
